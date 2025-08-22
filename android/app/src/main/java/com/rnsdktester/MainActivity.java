2087e1e6d2662367ce38deff0ab69160668aad2c
bogdan-khlyan <es.bogdan.hlyan@gmail.com>
2025-08-22 23:17:19 +0300

    refactor

--- android/app/src/main/java/com/rnsdktester/MainActivity.java
+++ android/app/src/main/java/com/rnsdktester/MainActivity.java
@@ -1,4 +1,4 @@
-package com.rnsdktester;
+package com.Voxport;
 
 import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
@@ -13,7 +13,7 @@ public class MainActivity extends ReactActivity {
    */
   @Override
   protected String getMainComponentName() {
-    return "RNSDKTester";
+    return "Voxport";
   }
 
   /**
